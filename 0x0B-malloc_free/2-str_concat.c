9d1fd9342d08aa5b84039b8d8cf8e14c3527f16a
gilbert <gilbertkamau32@gmail.com>
2023-03-15 12:51:52 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,43 +1,33 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <main.h>
 
-/**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
- */
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, ci;
+	int i, j, k;
+	char *newStr = NULL;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-		i = ci = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
+	for (i = 0; s1[i] != '\0'; i++)
+	;
 
-	if (conct == NULL)
-		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
-		conct[i] = s1[i];
-		i++;
-	}
+	for (j = 0; s2[j] != '\0'; j++)
+	;
 
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-	conct[i] = '\0';
-	return (conct);
+	newStr = (char *)malloc((i + j) * sizeof(char));
+
+	if (newStr == NULL)
+	return (NULL);
+
+	for (k = 0; s1[k] != '\0'; k++)
+	newStr[k] = s1[k];
+
+	for (k = 0; s2[k] != '\0'; k++)
+	newStr[k + i] = s2[k];
+
+	return (newStr);
 }
